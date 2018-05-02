085a2fd29f44ebf9fcc9ff0cd98cc43f232e9231
liyang <356514033@qq.com>
2018-05-02 23:35:04 +0800

    update restfulApi.js

--- server/restfulApi.js
+++ server/restfulApi.js
@@ -14,11 +14,11 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 
 //
 app.all('*', function (req, res, next) {
-  res.header("Content-Type", "application/json;charset=utf-8");
+  // res.header("Content-Type", "application/json;charset=utf-8");
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By", ' 3.2.1')
+  // res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  // res.header("X-Powered-By", ' 3.2.1')
   if (req.method == "OPTIONS") res.send(200);/*让options请求快速返回*/
   else next();
 });
@@ -33,6 +33,7 @@ app.get('/getTest', function (req, res) {
 
 app.post('/postTest', function (req, res) {
   console.log(req.body);
+  res.json({"msg": "application/json"});
   res.end();
 })
 
