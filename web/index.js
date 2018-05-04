99758849734abaebca3d1aafbd85e3b78c312224
liyang <356514033@qq.com>
2018-05-04 17:01:00 +0800

    This is allows your send POST and GET requests

--- web/index.js
+++ web/index.js
@@ -1,22 +1,88 @@
-const ajaxLy = (reqType, url, params, callback) => {
-  let xmlHttp = '',
-      contentType = 'application/json';
-  if(window.XMLHttpRequest){
-    xmlHttp = new XMLHttpRequest();
-  }else {
-    xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
-  }
-  xmlHttp.onreadystatechange = function() {
-    if(xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-      callback(xmlHttp.responseText);
+function Ajax() {
+  this.defaultContentType = 'application/x-www-form-urlencoded';
+}
+/*
+*@param { String, Object, String } url and params of http. data type of request
+*@return { Promise } 
+*/
+Ajax.prototype.post = function (url, params, dataType) {
+  var promise = new Promise((resolve, reject) => {
+    var xmlHttp = '';
+    if (window.XMLHttpRequest) {
+      xmlHttp = new XMLHttpRequest();
+    } else {
+      xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
     }
+    xmlHttp.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        if (this.status === 200)
+          resolve(this.responseText)
+        else if (this.status !== 0)
+          reject(new Error(this.status));
+      }
+    }
+    xmlHttp.open('POST', url, true);
+    if (dataType === 'json') this.defaultContentType = 'application/json';
+    xmlHttp.setRequestHeader('Content-type', this.defaultContentType);
+    if(this.defaultContentType == 'application/json') xmlHttp.send(JSON.stringify(params));
+    else xmlHttp.send(this.getUrlencoeded(params));
+  })
+  return promise;
+}
+/*
+*@param { String, Object } url and params of http.
+*@return { Promise } 
+*/
+Ajax.prototype.get = function (url, params) {
+  var promise = new Promise((resolve, reject) => {
+    var xmlHttp = '';
+    if (window.XMLHttpRequest) {
+      xmlHttp = new XMLHttpRequest();
+    } else {
+      xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
+    }
+    xmlHttp.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        if (this.status === 200)
+          resolve(this.responseText)
+        else if (this.status !== 0)
+          reject(new Error(this.status));
+      }
+    }
+    xmlHttp.open('GET', `${url}?${this.getUrlencoeded(params)}`, true);
+    xmlHttp.send();
+  })
+  return promise;
+}
+/*
+*@param { Object } url and params of http
+*@return { String } return urlencoeded string
+*/
+Ajax.prototype.getUrlencoeded = function (params) {
+  var str = '';
+  for (let [k, v] of Object.entries(params)) {
+    str += (k + '=' + v + '&')
   }
-  xmlHttp.open(reqType, url, true);
-  // xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  xmlHttp.setRequestHeader('Content-type', contentType);
-  xmlHttp.send(JSON.stringify(params));
-  // xmlHttp.send('msg=send&id=1')
+  str = str.slice(0, -1);
+  return str;
 }
 
-ajaxLy('post', "http://localhost:9090/postTest", { id: 1, name: 'liyang' }, (val) => {console.log(val)} );
-// ajaxLy("http://localhost:9090/getTest", (val) => { console.log(val) });
+function init() {
+   return new Ajax()
+}
+
+var ajax = init();
+
+
+
+ajax.get("http://localhost:9090/getTest", { id: 1, name: 'liyang' }, 'json').then((res) => {
+  console.log(res);
+}).catch(err => {
+  console.log(err);
+})
+
+ajax.post("http://localhost:9090/postTest", { id: 1, name: 'liyang' }, 'json').then((res) => {
+  console.log(res);
+}).catch(err => {
+  console.log(err);
+})
